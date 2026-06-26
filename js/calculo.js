6567305e9bcbf792972d1a05d8dafc6f78ecc0d3
Joao Tabosa <joao.v.souza318@aluno.senai.br>
2026-06-26 20:25:10 -0300

    Codificação Completa, Código JS 100% funcional

--- js/calculo.js
+++ js/calculo.js
@@ -1,24 +1,26 @@
 export function valorTotal(valorUnitario, quantidade) {
-    return valorUnitario * quantidade
+    return valorUnitario * quantidade;
 }
 
 export function calcular(descricao, valorTotal) {
+
     if (valorTotal <= 3000) {
-        return "Isento"
+        return "Item Isento de Valores Adicionais";
 
-    } else if (valorTotal > 3000 && valorTotal <= 8000) {
-        const valorAdicional = valorTotal * 0.05
-        return `Produto: ${descricao} | Valor adicional: R$ ${valorAdicional.toFixed(2)}`
+    } else if (valorTotal <= 8000) {
+        const valorAdicional = valorTotal * 0.05;
+        return `Uma Taxa de 5% será Aplicada | Valor Adicional: R$ ${valorAdicional.toFixed(2)}`;
 
-    } else if (valorTotal > 8000 && valorTotal <= 12000) {
-        const valorAdicional = valorTotal * 0.10
-        return `<p>Produto: ${descricao.value} | Valor adicional: R$ ${valorAdicional.toFixed(2)}</p>`
+    } else if (valorTotal <= 12000) {
+        const valorAdicional = valorTotal * 0.10;
+        return `Uma Taxa de 10% será Aplicada | Valor Adicional: R$ ${valorAdicional.toFixed(2)}`;
 
-    } else if (valorTotal > 12000 && valorTotal <= 20000) {
-        const valorAdicional = valorTotal * 0.15
-        return `<p>Produto: ${descricao.value} | Valor adicional: R$ ${valorAdicional.toFixed(2)}</p>`
-    
-    } else if (valorTotal > 20000) {
-        const valorAdicional = valorTotal * 0.20
-        return `<p>Produto: ${descricao.value} | Valor adicional: R$ ${valorAdicional.toFixed(2)}</p>`
-    }}
+    } else if (valorTotal <= 20000) {
+        const valorAdicional = valorTotal * 0.15;
+        return `Uma Taxa de 15% será Aplicada | Valor Adicional: R$ ${valorAdicional.toFixed(2)}`;
+
+    } else {
+        const valorAdicional = valorTotal * 0.20;
+        return `Uma Taxa de 20% será Aplicada | Valor Adicional: R$ ${valorAdicional.toFixed(2)}`;
+    }
+}
--- js/produto.js
+++ js/produto.js
@@ -1,62 +1,54 @@
-/*
-    Até R$ 3000,00 Isento
-    Entre R$ 3000,00 até R$ 8.000,00 5%
-    Entre R$ 8.000,00 até R$ 12.000,00 10%
-    Entre R$ 12.000,00 até R$ 20.000,00 15%
-    Acima de R$ 20.000,00 20%
+import * as func from "./calculo.js";
 
-    Sua tarefa é desenvolver um algoritmo utilizando as tecnologias HTML e JavaScript para atender a esse desafio.
-    O sistema deverá conter um formulário que permita ao usuário informar os seguintes dados do lote:
-    Descrição do Produto; Valor unitário; Quantidade.
-
-    Após o envio do formulário, o sistema deverá exibir, na mesma página e abaixo do formulário, uma lista contendo:
-    Valor adicional (ou a informação "Isento", quando aplicável); A lista deverá ser atualizada automaticamente a cada
-    novo produto cadastrado, preservando os registros inseridos anteriormente. 
-    Fica a seu critério mas é obrigatório utilizar: Array, Funções, Estrutura de repetição e Estrutura de Decisão.
-*/
-import * as func from "./calculo.js"
-
-const produtos = []
-const produtoForm = document.querySelector("#produtoForm")
-const resultado = document.querySelector("#div-resultado")
+const produtos = [];
+const produtoForm = document.querySelector("#produtoForm");
+const resultado = document.querySelector("#div-resultado");
 
 produtoForm.addEventListener("submit", (evt) => {
-    evt.preventDefault()
-    
-    const dados = new FormData(produtoForm)
+    evt.preventDefault();
+
+    const dados = new FormData(produtoForm);
 
     const produto = {
         descricao: dados.get("nomeP"),
-        valorUnitario: dados.get("valorUni"),
-        quantidade: dados.get("quantidade"),
-    }
+        valorUnitario: Number(dados.get("valorUni")),
+        quantidade: Number(dados.get("quantidade")),
+    };
 
-    const valorTotal = func.valorTotal(produto.valorUnitario, produto.quantidade)
-    const situacao = func.calcular(produto.descricao, produto.valorTotal)
-    
     const listaProdutos = () => {
-        resultado.innerHTML = ''
+        resultado.innerHTML = "";
+
         produtos.forEach((elem, i) => {
 
+            const valorTotal = func.valorTotal(
+                elem.valorUnitario,
+                elem.quantidade
+            );
+
+            const situacao = func.calcular(
+                elem.descricao,
+                valorTotal
+            );
+
             resultado.innerHTML += `
-            <br>
-                ================ Dados do Produto ${i+1} ================    <br>
-                Descrição do Produto: ${elem.descricao} <br>
-                Valor Unitário do Produto: ${elem.valorUnitario} <br>
-                Quantidade de Produtos: ${elem.quantidade} <br>
-                Valor Total do Produto: R$ ${valorTotal} <br>
-                Situação: ${situacao}
                 <br>
-                <br>
+                ================ Dados do Produto ${i + 1} ================<br>
+                Descrição do Produto: ${elem.descricao}<br>
+                Valor Unitário do Produto: R$ ${elem.valorUnitario.toFixed(2)}<br>
+                Quantidade de Produtos: ${elem.quantidade}<br>
+                Valor Total do Produto considerando Quantidade: R$ ${valorTotal.toFixed(2)}<br>
+                ${situacao}
+                <br><br>
             `;
-        })
-    }
+        });
+    };
 
-    const addListaProdutos = (objetoProduto) => {   
-            produtos.push(objetoProduto)
-            listaProdutos()
-        }
-    
-    addListaProdutos(produto)
+    const addListaProdutos = (objetoProduto) => {
+        produtos.push(objetoProduto);
+        listaProdutos();
+    };
 
-})
+    addListaProdutos(produto);
+
+    produtoForm.reset();
+});
